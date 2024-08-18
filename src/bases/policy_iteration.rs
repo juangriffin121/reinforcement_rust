e4438f60820e8f7ba82b76e9dca4414457ae886d
griffin <juanogriffin@gmail.com>
2024-08-18 18:42:25 -0300

    solved ex 4.9

--- src/bases/policy_iteration.rs
+++ src/bases/policy_iteration.rs
@@ -15,10 +15,9 @@ where
     S: mdp::State,
     E: mdp::Enviorment<S, A>,
 {
-    let mut delta: f32 = 0.0;
-
     loop {
-        for state in states {
+        let mut delta: f32 = 0.0;
+        for (i, state) in states.iter().enumerate() {
             let v = values[state];
             let value = match &agent.policy {
                 mdp::Policy::Deterministic(policy) => {
@@ -46,17 +45,20 @@ where
             delta = delta.max((v - value).abs());
             let val = values.get_mut(state).unwrap();
             *val = value;
+            // println!("state {i:?}");
+            // println!("delta {delta:?}");
         }
         if delta < tolerance {
             break;
         }
     }
+    println!("end pol eval");
     values
 }
 
 pub fn policy_improvement<E, S, A>(
     agent: &mut mdp::Agent<S, A>,
-    enviorment: E,
+    enviorment: &E,
     states: &Vec<S>,
     gamma: f32,
     tolerance: f32,
@@ -67,8 +69,8 @@ where
     S: mdp::State,
     E: mdp::Enviorment<S, A>,
 {
-    let mut policy_stable = false;
-    for state in states {
+    let mut policy_stable = true;
+    for (i, state) in states.iter().enumerate() {
         match &mut agent.policy {
             mdp::Policy::Deterministic(policy) => {
                 let old_action = policy[&state].clone();
@@ -87,7 +89,7 @@ where
                     };
                 }
                 let action = policy.get_mut(&state).unwrap();
-                *action = max_action;
+                *action = max_action.clone();
                 if *action != old_action {
                     policy_stable = false;
                 }
@@ -98,15 +100,17 @@ where
         }
     }
     if policy_stable {
+        println!("pol improv returns");
         return values;
     }
-    values = policy_evaluation(agent, &enviorment, states, values, gamma, tolerance);
+    values = policy_evaluation(agent, enviorment, states, values, gamma, tolerance);
     return policy_improvement(agent, enviorment, states, gamma, tolerance, values);
 }
 
 pub fn policy_iteration<E, S, A>(
     agent: &mut mdp::Agent<S, A>,
-    enviorment: E,
+    enviorment: &E,
+    values: Option<HashMap<S, f32>>,
     gamma: f32,
     tolerance: f32,
 ) -> HashMap<S, f32>
@@ -115,24 +119,28 @@ where
     S: mdp::State,
     E: mdp::Enviorment<S, A>,
 {
-    let mut rng = thread_rng();
-    let mut values: HashMap<S, f32> = HashMap::new();
-    let states = enviorment.get_states();
-    for state in &states {
-        if state.is_terminal() {
-            values.insert(state.clone(), 1.0);
-        } else {
-            values.insert(state.clone(), rng.gen());
+    let mut values = values.unwrap_or({
+        let mut rng = thread_rng();
+        let mut values: HashMap<S, f32> = HashMap::new();
+        let states = enviorment.get_states();
+        for state in &states {
+            if state.is_terminal() {
+                values.insert(state.clone(), 1.0);
+            } else {
+                values.insert(state.clone(), rng.gen());
+            }
         }
-    }
-    values = policy_evaluation(agent, &enviorment, &states, values, gamma, tolerance);
+        values
+    });
+    let states = enviorment.get_states();
+    values = policy_evaluation(agent, enviorment, &states, values, gamma, tolerance);
     return policy_improvement(agent, enviorment, &states, gamma, tolerance, values);
 }
 
 pub fn value_iteration<E, S, A>(
     enviorment: &E,
     states: &Vec<S>,
-    mut values: HashMap<S, f32>,
+    values: Option<HashMap<S, f32>>,
     gamma: f32,
     tolerance: f32,
 ) -> HashMap<S, f32>
@@ -141,6 +149,19 @@ where
     S: mdp::State,
     E: mdp::Enviorment<S, A>,
 {
+    let mut values = values.unwrap_or({
+        let mut rng = thread_rng();
+        let mut values: HashMap<S, f32> = HashMap::new();
+        let states = enviorment.get_states();
+        for state in &states {
+            if state.is_terminal() {
+                values.insert(state.clone(), 0.0);
+            } else {
+                values.insert(state.clone(), rng.gen());
+            }
+        }
+        values
+    });
     loop {
         let mut delta: f32 = 0.0;
         for (i, state) in states.iter().enumerate() {
@@ -180,4 +201,36 @@ where
         }
     }
     values
+}
+
+pub fn greedy_policy<S, A, E>(
+    enviorment: &E,
+    states: &Vec<S>,
+    values: &HashMap<S, f32>,
+    gamma: f32,
+) -> HashMap<S, A>
+where
+    A: mdp::Action,
+    S: mdp::State,
+    E: mdp::Enviorment<S, A>,
+{
+    let mut policy: HashMap<S, A> = HashMap::new();
+    for state in states {
+        let actions = enviorment.posible_actions(&state);
+        let mut max_action: A = actions[0].clone();
+        let mut max_action_value = -f32::INFINITY;
+        for action in actions {
+            let probs = enviorment.dynamics(&state, &action);
+            let mut sum = 0.0;
+            for ((next_state, reward), prob) in probs.iter() {
+                sum += prob * (*reward as f32 + gamma * values[next_state])
+            }
+            if max_action_value < sum {
+                max_action = action;
+                max_action_value = sum;
+            };
+        }
+        policy.insert(state.clone(), max_action);
+    }
+    policy
 }
--- src/exercises/ex4_3.rs
+++ src/exercises/ex4_3.rs
@@ -23,13 +23,14 @@ Show your results graphically, as in Figure 4.3. Are your results stable as Î¸ â
 
 use std::collections::HashMap;
 
-use rand::{thread_rng, Rng};
-
 use crate::bases::{
     mdp::{Action, Agent, Enviorment, Policy, State},
-    policy_iteration::value_iteration,
+    policy_iteration::{greedy_policy, policy_iteration, value_iteration},
 };
+use plotters::prelude::*;
+use rand::{thread_rng, Rng};
 
+use rand::seq::SliceRandom;
 #[derive(PartialEq, Eq, Clone, Hash, Debug)]
 pub struct GamblerState {
     capital: u8,
@@ -41,7 +42,7 @@ pub struct GamblerAction {
 }
 
 pub struct Casino {
-    probability_of_win: f32,
+    probability_of_win: f32, //ph
 }
 
 impl Enviorment<GamblerState, GamblerAction> for Casino {
@@ -52,9 +53,9 @@ impl Enviorment<GamblerState, GamblerAction> for Casino {
     ) -> HashMap<(GamblerState, i32), f32> {
         let mut distribution: HashMap<(GamblerState, i32), f32> = HashMap::new();
         // since i loop over the dict to do the sum and multiply by prob and multiply by zero and
-        // sum is like not doing anything
+        // sum is like not doing anything theres no need to add the 0 prob to the hashmap
         for next_state in self.get_states() {
-            if state.capital == 100 {
+            if state.is_terminal() {
                 continue;
             }
             if next_state.capital == state.capital - action.stake {
@@ -84,7 +85,7 @@ impl Enviorment<GamblerState, GamblerAction> for Casino {
         posible_actions
     }
     fn get_states(&self) -> Vec<GamblerState> {
-        let possible_capitals = 0..100;
+        let possible_capitals = 0..101;
         let mut states: Vec<GamblerState> = Vec::new();
         for capital in possible_capitals {
             states.push(GamblerState { capital })
@@ -95,28 +96,141 @@ impl Enviorment<GamblerState, GamblerAction> for Casino {
 
 impl State for GamblerState {
     fn is_terminal(&self) -> bool {
-        self.capital == 100
+        self.capital == 0 || self.capital == 100
     }
 }
 impl Action for GamblerAction {}
 
+fn plot_graph(
+    values: HashMap<GamblerState, f32>,
+    file_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Extract and sort the data from the HashMap
+    let mut data: Vec<(u8, f32)> = values.into_iter().map(|(k, v)| (k.capital, v)).collect();
+    data.sort_by_key(|&(attr, _)| attr);
+
+    // Create a chart
+    let root = BitMapBackend::new(file_path, (800, 600)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Values", ("sans-serif", 50).into_font())
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .margin(20)
+        .build_cartesian_2d(
+            data.first().unwrap().0 as f64..data.last().unwrap().0 as f64,
+            0f64..data.iter().map(|(_, v)| *v as f64).fold(f64::NAN, f64::max),
+        )?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            data.into_iter().map(|(x, y)| (x as f64, y as f64)),
+            &RED,
+        ))?
+        .label("Curve")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 10, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
+}
+
+fn plot_graph_act(
+    policy: HashMap<GamblerState, GamblerAction>,
+    file_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Extract and sort the data from the HashMap
+    let mut data: Vec<(u8, u8)> = policy
+        .into_iter()
+        .map(|(k, v)| (k.capital, v.stake))
+        .collect();
+    data.sort_by_key(|&(attr, _)| attr);
+
+    // Create a chart
+    let root = BitMapBackend::new(file_path, (800, 600)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Policy", ("sans-serif", 50).into_font())
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .margin(20)
+        .build_cartesian_2d(
+            data.first().unwrap().0 as f64..data.last().unwrap().0 as f64,
+            0f64..data.iter().map(|(_, v)| *v as f64).fold(f64::NAN, f64::max),
+        )?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            data.into_iter().map(|(x, y)| (x as f64, y as f64)),
+            &RED,
+        ))?
+        .label("Curve")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 10, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
+}
+
 pub fn solution() -> HashMap<GamblerState, f32> {
     let mut rng = thread_rng();
-    let mapping: HashMap<GamblerState, GamblerAction> = HashMap::new();
-    let mut _gambler = Agent::<GamblerState, GamblerAction> {
-        policy: Policy::Deterministic(mapping),
-    };
+
     let casino = Casino {
         probability_of_win: 0.4,
     };
     let states = casino.get_states();
+
+    let mut mapping: HashMap<GamblerState, GamblerAction> = HashMap::new();
+    for state in &states {
+        let actions = casino.posible_actions(&state);
+        if let Some(action) = actions.choose(&mut rng) {
+            mapping.insert(state.clone(), action.clone());
+        } else {
+            println!("The vector is empty!");
+        }
+    }
+
+    let mut gambler = Agent::<GamblerState, GamblerAction> {
+        policy: Policy::Deterministic(mapping),
+    };
     let mut values: HashMap<GamblerState, f32> = HashMap::new();
     for state in &states {
         if state.is_terminal() {
-            values.insert(state.clone(), 1.0);
+            values.insert(state.clone(), 0.0);
         } else {
             values.insert(state.clone(), rng.gen());
         }
     }
-    value_iteration(&casino, &states, values, 1.0, 0.1)
+    let gamma = 1.0;
+    let tolerance = 0.01;
+    let values = value_iteration(&casino, &states, Some(values), gamma, tolerance);
+    plot_graph(values.clone(), "graph.png").unwrap();
+    let policy = greedy_policy(&casino, &states, &values, gamma);
+    plot_graph_act(policy, "act_graph.png").unwrap();
+
+    println!("done with val iter");
+
+    let values = policy_iteration(&mut gambler, &casino, None, gamma, tolerance);
+    plot_graph(values.clone(), "graph,_PI.png").unwrap();
+    let policy = greedy_policy(&casino, &states, &values, gamma);
+    plot_graph_act(policy, "act_graph_PI.png").unwrap();
+    values
 }
